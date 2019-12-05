ea89495fb24393a03ed013f2ac6b94a2d833dd56
Everardo Gonzalez <everardo.a.gonzalez@gmail.com>
2019-12-05 15:13:33 -0500

    updated with custom step function - doesnt work, instead it ends episodes very quickly and the reward is always super low

--- mountain_car.py
+++ mountain_car.py
@@ -1,3 +1,8 @@
+"""
+All original code from https://gist.github.com/gkhayes/3d154e0505e31d6367be22ed3da2e955
+Written by gkhayes
+"""
+
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
@@ -7,11 +12,42 @@ env = gym.make('MountainCar-v0')
 # env = gym.make('CartPole-v1')
 env.reset()
 
+
+def custom_step(env, action):
+    import math
+    assert env.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+
+    position, velocity = env.state
+    velocity += (action-1)*env.force + math.cos(3*position)*(-env.gravity)
+    velocity = np.clip(velocity, -env.max_speed, env.max_speed)
+    position += velocity
+    position = np.clip(position, env.min_position, env.max_position)
+    if (position==env.min_position and velocity<0): velocity = 0
+
+    done = bool(position >= env.goal_position and velocity >= env.goal_velocity)
+    # print(done)
+    reward = -1.0
+
+    env.state = (position, velocity)
+    return np.array(env.state), reward, done, {}
+
 # Define Q-learning function
 def QLearning(env, learning, discount, epsilon, min_eps, episodes):
     # Determine size of discretized state space
     num_states = (env.observation_space.high - env.observation_space.low)*\
                     np.array([10, 100])
+    print('env observation space diff: ',(env.observation_space.high - env.observation_space.low))
+    print('num_states: ',num_states)
+
+    # it looks like num states is trying to discretize the number of states the car can be in
+    # from a continuous state space to a discrete state space
+
+    # the observation space is constrained by the agents min position ,max position
+    # as well as min speed and max speed
+
+    print('env.observation_space:', env.observation_space)
+    print('env observation space high:',env.observation_space.high)
+    print('env observation space low:', env.observation_space.low)
     print('num_states:', num_states)
     num_states = np.round(num_states, 0).astype(int) + 1
     print('num_states:',num_states)
@@ -20,6 +56,10 @@ def QLearning(env, learning, discount, epsilon, min_eps, episodes):
     Q = np.random.uniform(low = -1, high = 1,
                           size = (num_states[0], num_states[1],
                                   env.action_space.n))
+    # action space contains 0,1,2 - Discrete(3) object has 3 states
+    # print(Q.shape)
+    # Q table is 19x15x3, 19x15 states, 3 actions
+
 
     # Initialize variables to track rewards
     reward_list = []
@@ -28,8 +68,8 @@ def QLearning(env, learning, discount, epsilon, min_eps, episodes):
     # Calculate episodic reduction in epsilon
     reduction = (epsilon - min_eps)/episodes
 
-    import sys
-    sys.exit(0)
+    # import sys
+    # sys.exit(0)
 
     # Run Q learning algorithm
     for i in range(episodes):
@@ -55,6 +95,7 @@ def QLearning(env, learning, discount, epsilon, min_eps, episodes):
 
             # Get next state and reward
             state2, reward, done, info = env.step(action)
+            # state2, reward, done, info = custom_step(env, action)
 
             # Discretize state2
             state2_adj = (state2 - env.observation_space.low)*np.array([10, 100])
